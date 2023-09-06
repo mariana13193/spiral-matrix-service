4bb5e5806c290a72917365692d0a75dfabcf80ad
mariana13193 <mariana13193@gmail.com>
2023-09-06 13:30:44 -0300

    created fib series and unit test

--- src/main/java/com/challenge/spiralmatrixservice/service/MatrixService.java
+++ src/main/java/com/challenge/spiralmatrixservice/service/MatrixService.java
@@ -7,11 +7,30 @@ import java.util.Arrays;
 @Service
 public class MatrixService {
 
+    public int[] fibonacci;
+
     public int[][] getFibonacciSpiralMatrix(int rows, int cols){
+        int size = rows*cols;  //TODO validations
+
+        //Create Fibonacci Series
+        fib(size);
+
+
+
         int[][] response = new int[rows][cols];
         for(int[] arr:response){
             Arrays.fill(arr,1);
         }
         return response;
+    }
+
+    public void fib(int n) {
+        fibonacci = new int[n+2];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
     }
 }
---
+++ src/test/java/com/challenge/spiralmatrixservice/MatrixServiceTest.java
@@ -0,0 +1,30 @@
+package com.challenge.spiralmatrixservice;
+
+import com.challenge.spiralmatrixservice.service.MatrixService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MatrixServiceTest {
+
+private MatrixService matrixService;
+
+    @BeforeEach
+    public void setUp() {
+        this.matrixService = new MatrixService();
+    }
+
+    @Test
+    public void fib_series_square_is_ok(){
+        int size = 16;
+        matrixService.fib(size);
+        for (int i = 0; i <= size; i++)
+            System.out.print(matrixService.fibonacci[i] + " ");
+        assertNotNull(matrixService.fibonacci);
+        assertEquals(matrixService.fibonacci.length,size+2);
+    }
+}
